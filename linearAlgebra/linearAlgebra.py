8745e92627020fa6928b46bc6ff3eafde165a4ee
torchhound <jcieslik@umail.iu.edu>
2016-11-12 16:32:51 -0500

    Finished linearAlgebra

--- linearAlgebra/linearAlgebra.py
+++ linearAlgebra/linearAlgebra.py
@@ -1,4 +1,7 @@
 import functools
+import math
+
+#Vectors and scalars
 
 def vectroAdd(v, w):
 	'''Adds corresponding elements'''
@@ -24,4 +27,31 @@ def dot(v, w):
 	'''v1 * w1 + ... + vN * wN'''
 	return sum(vI * wI for vI, wI in zip(v, w))
 
+def sumOfSquares(v):
+	'''v1 * v1 + ... + vN * vN'''
+	return dot(v, v)
 
+def magnitude(v):
+	return math.sqrt(sumOfSquares(v))
+
+def distance(v, w):
+	return magnitude(vectorSub(v, w))
+
+#Matrices
+
+def shape(A):
+	numRows = len(A)
+	numCols = len(A[0]) if A else 0
+	return numRows, numCols
+
+def getRow(A, i):
+	return A[i]
+
+def getColumn(A, j):
+	return [Ai[j] for Ai in A]
+
+def makeMatrix(rows, cols, entry):
+	return [[entry(i, j) for j in range(cols)] for i in range(rows)]
+
+def isDiagonal(i, j):
+	return 1 if i == j else 0
